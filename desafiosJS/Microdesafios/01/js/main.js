de053708cc52be84d8e0b9b75e21b2e8f827c694
Edem30 <138921897+Edem30@users.noreply.github.com>
2024-01-16 11:55:17 -0300

    atualizaçao imc

--- desafiosJS/Microdesafios/01/js/main.js
+++ desafiosJS/Microdesafios/01/js/main.js
@@ -1,2 +1,23 @@
 const lampada=document.getElementById('lampada');
-const 
+const ligar=document.getElementById('ligar');
+const desligar=document.getElementById('desliga');
+
+
+function ligando(){
+    lampada.src = './img/ligada.jpg';
+    
+}
+function desligando (){
+    lampada.src = './img/desligada.jpg';
+    
+}
+function quebrar(){
+    lampada.src = './img/quebra.jpg';
+    
+}
+
+ligar.addEventListener('click',ligando);
+desligar.addEventListener('click',desligando);
+lampada.addEventListener('mouseover',ligando);
+lampada.addEventListener('mouseleave',desligando)
+lampada.addEventListener('dblclick',quebrar);
